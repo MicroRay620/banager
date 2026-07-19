2e78b4ae16e647f0ac2c85b909b933aa69a71d4d
Ruby Rose <microray620@gmail.com>
2026-07-19 01:08:32 -0400

    Added an enviroment variable

--- commands/enviroment.sh
+++ commands/enviroment.sh
@@ -32,3 +32,4 @@ if ! command -v nix &>/dev/null; then
     export XDG_DATA_HOME="$HOME/.local/share"
     export XDG_STATE_HOME="$HOME/.local/state"
 fi
+export bastore="$HOME/.local/share/banager/deps"
