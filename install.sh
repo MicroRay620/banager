881359b92e0cce48656304316cce00da0b449beb
Ruby Rose <microray620@gmail.com>
2026-07-18 21:22:23 -0400

    Updated the install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ for super in "${supers[@]}"; do
 done
 "$SUPER" cd /tmp/ || exit 1
 git clone https://codeberg.org/RubyRose/banager.git 
-rm -rf banager/install.sh
+rm -rf banager/install.sh banager/docs/
 "$SUPER" mv banager "${XDG_CONFIG_HOME:-$HOME/.config}"
 echo -e "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager\"" >> "$HOME"/.bashrc
