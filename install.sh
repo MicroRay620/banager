1522358387a49f5904c93c40a9dde718e89a3f72
Ruby Rose <microray620@gmail.com>
2026-07-18 22:33:46 -0400

    Updated the config file

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-supers=( "sudo" "doas" )
-for super in "${supers[@]}"; do 
-    if command -v "$super" &>/dev/null; then 
-        SUPER="$super"
-    fi
-done
-"$SUPER" cd /tmp/ || exit 1
+cd /tmp/ || exit 1
+echo ls ./
+echo "Cloning in /tmp..."
 git clone https://codeberg.org/RubyRose/banager.git 
+echo "Removing dev and useless files and folders..."
 rm -rf banager/install.sh banager/docs/
-"$SUPER" mv banager "${XDG_CONFIG_HOME:-$HOME/.config}"
-echo -e "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager\"" >> "$HOME"/.bashrc
+echo "Making the config file..."
+mv ./banager "${XDG_CONFIG_HOME:-$HOME/.config}"
+if ! grep -Fq "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager/config\""; then
+    echo "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager/config\"" >> "$HOME"/.bashrc
+fi
+echo "Checking the plugins..."
+banager_config="${XDG_CONFIG_HOME:-$HOME/.config}/banager"
+command -v pacman &>/dev/null || rm -f "$banager_config/plugins/archlinux.plugin.sh"
+command -v nix &>/dev/null || rm -f "$banager_config/plugins/nixos.plugin.sh"
+echo "Installed."
+exit 0
