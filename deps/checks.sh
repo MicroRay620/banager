0c2870b57c6c326978df220238e4191c3e753bc7
Ruby Rose <microray620@gmail.com>
2026-07-18 23:10:53 -0400

    Fixed the issue where the terminal will crash upon opening

--- deps/checks.sh
+++ deps/checks.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/banager"
 data_dir="${XDG_DATA_HOME:-$HOME/.local/share}/banager"
+mkdir "$config_dir/user"
 # Commands
 for cmd_file in "$data_dir"/commands/*; do
     if [ -f "$cmd_file" ]; then
@@ -15,4 +16,3 @@ for plugin_file in "$config_dir"/plugins/*.plugin*; do
     # shellcheck disable=SC1090
     [ -f "$plugin_file" ] && source "$plugin_file"
 done
-mkdir "$config_dir/user"
