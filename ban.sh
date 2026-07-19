eb14951318b4d55a609b0038473cc98fdb9fd1ed
Ruby Rose <microray620@gmail.com>
2026-07-19 01:15:46 -0400

    Hopefully got the whole thing working

--- ban.sh
+++ ban.sh
@@ -1,2 +1,24 @@
-#!/bin/sh
-source "$HOME/.config/banager/bananger.conf"
+#!/bin/bash
+shopt -s nullglob
+# shellcheck disable=SC1091
+source "$HOME/.config/banager/banager.conf"
+source "$HOME/.config/banager/ban.sh"
+# TODO: Make these not be in the config file
+if [ "$external_source" = true ]; then
+    # shellcheck disable=SC1091
+    source -- "$(blesh-share)"/ble.sh --attach=none
+    # Uncomment which everone you want to use and have installed
+    if command -v thefuck &>/dev/null; then
+        eval "$(thefuck --alias -l SHELL_LOGGER)"
+    elif command -v pay-respects &>/dev/null; then
+        eval "$(pay-respects bash)"
+    fi
+    eval "$(starship init bash)"
+    # TODO: Make a check and have it in the template nixos config for starship and zoxide working
+    if command -v zoxide &>/dev/null; then 
+        eval "$(zoxide init --cmd "cd" bash)"
+    fi
+fi
+if [ "$flag_display" = true ]; then 
+    Flag 
+fi
