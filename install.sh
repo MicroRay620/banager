0d1c8e2d64eb4e303d499fa89299709fe0aabbe4
Ruby Rose <microray620@gmail.com>
2026-07-19 00:44:10 -0400

    Maybe fixed the install issue

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ mv ./banager/deps/ ./banager/commands/ "$HOME"/.local/share/banager
 mv ./banager "${XDG_CONFIG_HOME:-$HOME/.config}"
 mv "$HOME"/.bashrc "$HOME"/.bashrc.bak
 touch "$HOME"/.bashrc
-if ! grep -Fq "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager/config\""; then
+if ! grep "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager/config\""; then
     echo -e "#!/bin/bash\nsource -- \"$HOME/.config/banager/config\"" >> "$HOME"/.bashrc
 fi
 echo "Checking the plugins..."
