7e79ccc69bb8d99431ec711f840c468e6bffa604
Ruby Rose <microray620@gmail.com>
2026-07-18 21:19:33 -0400

    Added install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+supers=( "sudo" "doas" )
+for super in "${supers[@]}"; do 
+    if command -v "$super" &>/dev/null; then 
+        SUPER="$super"
+    fi
+done
+"$SUPER" cd /tmp/ || exit 1
+git clone https://codeberg.org/RubyRose/banager.git 
+rm -rf banager/install.sh
+"$SUPER" mv banager "${XDG_CONFIG_HOME:-$HOME/.config}"
+echo -e "source -- \"${XDG_CONFIG_HOME:-$HOME/.config}/banager\"" >> "$HOME"/.bashrc
