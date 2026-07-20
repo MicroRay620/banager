be3c3fd5c975967d8b155d5a8ebb532f200149e1
Ruby Rose <microray620@gmail.com>
2026-07-20 12:08:51 -0400

    Added config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# For the variables used, `export` is needed. 
+# This is to ensure they can be accessed by the local options
+# export flag_display="true"
+# export enable_alias="true"
+# export distro_aliases="true"
+
+
+# Evals and Sources 
+# This is here for you imports and addition plugins that are made for bash and exclusively for banager. 
+# Such as blesh, thefuck, pay-respects, starship, and zoxide
+# Look at the docs for those tools for how to add them and look for the bash option.
