3a96f269cb0879f99221c12db024b7e6e8ed9a69
Ruby Rose <microray620@gmail.com>
2026-07-20 12:36:47 -0400

    Made numbers be the only valid input

--- plugins/gtrash.plugin.sh
+++ plugins/gtrash.plugin.sh
@@ -8,16 +8,31 @@ if [ ! -e "$prune" ]; then
     echo "Would you like to prune in days or size? "
     read -r prune_choice
     case "$prune_choice" in
-        *ay* | *AY*) 
+        *ay* | *AY*)
+            days_choice=""
             prune_denotion="--day"
-            echo "How many days? "
-            read -r days_choice
-            chosen_prune="$days_choice"
+            while true; do 
+                echo "How many days? "
+                read -r days_choice
+                if [[ "$days_choice" =~ ^[0-9]+$ ]]; then 
+                    chosen_prune="$days_choice"
+                    break 
+                else 
+                    echo "That isn't a number, please try again"
+                fi
+            done 
             ;;
-        *ize* | *IZE*) 
-            prune_denotion="--size"
-            echo "Size amount? "
-            read -r size_choice
+        *ize* | *IZE*)
+            while true; do 
+                prune_denotion="--size"
+                echo "Size amount? "
+                read -r size_choice
+                if [[ "$size_choice" =~ ^[0-9]+$ ]]; then 
+                    break 
+                else 
+                    echo "Not a valid size, try again"
+                fi
+            done 
             echo "Byte size? (In all caps and using the acronym, example: GB) "
             read -r byte_choice
             chosen_prune="$size_choice$byte_choice"
