34762b150996be66776535cca423297e8326c671
Ruby Rose <microray620@gmail.com>
2026-07-26 16:05:45 -0400

    Made a template plugin

---
+++ docs/template-plugin.plugin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#!banager/plugin
+# name = Template 
+# owner = <YOUR NAME>
+# description = <PLUGIN DESCRIPTION>
+# source = <SOURCE CODE LINK>
+# shellcheck source=/dev/null
+source -- "$XDG_DATA_HOME/banager/commands/declare.sh"
