f572b735f5a01900bd37a4157ce543cc7bc3cc67
Ruby Rose <microray620@gmail.com>
2026-07-26 16:55:00 -0400

    Updated the deps/config.sh file

--- deps/config.sh
+++ deps/config.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-# config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/banager"
 config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/banager"
 data_dir="${XDG_DATA_HOME:-$HOME/.local/share}/banager"
 cache_dir="${XDG_CACHE_HOME:-$HOME/.cache}/banager"
@@ -25,35 +24,46 @@ for cmd_file in "$data_dir"/commands/*; do
 done
 # Plugins
 # This is for managing the plugins
-for plugin_file in "$config_dir"/plugins/*.plugin*; do
-    plugin_path="${plugin_file##*\/}"
-    invalid_plugin="${plugin_path%%.plugin.sh}"
-    if [ -f "$plugin_file" ]; then 
-        # NOTE: This is here for debugging. 
-        # Uncomment this if you want to test your plugin
-        # echo "Reading $invalid_plugin.plugin.sh"
-        owner=$(grep "owner =" "$plugin_file" | cut -d'=' -f2-)
-        shebang=$(grep "#!banager/plugin" "$plugin_file")
-        if [ -n "$owner" ]; then
-            # shellcheck source=/dev/null
-            source "$plugin_file"
-            # NOTE: This is here for debugging. Uncomment this if you are debugging a plugin for Banager
-            # echo -e "$invalid_plugin is valid"
-            # echo -e "#!banager/plugin exists in $invalid_plugin"
-        else
-            echo "$invalid_plugin is invalid"
-            if [ -z "$shebang" ]; then 
-                # NOTE: This checks for #!banger/plugin to see if a plugin is valid
-                echo -e " \e[31m Cause: BC1: $invalid_plugin.plugin.sh's #!banager/plugin is missing\e[0m"
-            fi
-            if [ -z "$owner" ]; then 
-                echo -e " \e[31m Cause: BC2: $invalid_plugin.plugin.sh's 'owner' field is empty\e[0m"
-            else 
-                echo -e " \e[31mCause: BC1: $invalid_plugin.plugin.sh's 'owner' field is missing\e[0m"
+# shellcheck disable=SC2154
+if [ "$plugin" = "enable" ]; then
+    for plugin_file in "$config_dir"/plugins/*.plugin*; do
+        plugin_path="${plugin_file##*\/}"
+        invalid_plugin="${plugin_path%%.plugin.sh}"
+        if [ -f "$plugin_file" ]; then 
+            # NOTE: This is here for debugging. 
+            # Uncomment this if you want to test your plugin
+            # echo "Reading $invalid_plugin.plugin.sh"
+            owner=$(cat "$plugin_file" | grep "# owner =")
+            shebang=$(cat "$plugin_file" | grep "#!banager/plugin")
+            if [ -n "$owner" ]; then
+                # shellcheck source=/dev/null
+                source "$plugin_file"
+                # NOTE: This is here for debugging. Uncomment this if you are debugging a plugin for Banager
+                # echo -e "$invalid_plugin is valid"
+                # echo -e "#!banager/plugin exists in $invalid_plugin"
+            else
+                cat "$plugin_file"
+                echo "$invalid_plugin is invalid"
+                if [ -z "$shebang" ]; then 
+                    # NOTE: This checks for #!banger/plugin to see if a plugin is valid
+                    echo -e "\e[31m Cause: BC1: $invalid_plugin.plugin.sh's #!banager/plugin is missing\e[0m"
+                fi
+                if [ -z "$owner" ]; then 
+                    echo -e "\e[31m Cause: BC2: $invalid_plugin.plugin.sh's 'owner' field is empty\e[0m"
+                else 
+                    echo -e "\e[31mCause: BC1: $invalid_plugin.plugin.sh's 'owner' field is missing\e[0m"
+                fi
             fi
         fi
+    done
+    if [ -f "$config_dir/plugins/fetch.plugin.sh" ]; then 
+        Flag
     fi
-done
-if [ -f "$config_dir/plugins/fetch.plugin.sh" ]; then 
-    Flag
+    if [ -f "$config_dir/plugins/gtrash.plugin.sh" ]; then 
+        touch "$cache_dir/gtrash.sh" 
+        if command -v gtrash &>/dev/null; then 
+            gtrash completion bash >> "$cache_dir/gtrash.sh"
+        fi
+    fi
 fi
+
