e0bf78fe38c91d080110aa30235e430445658dfe
Ruby Rose <microray620@gmail.com>
2026-07-26 22:08:09 -0400

    Changed the order of the placement of the evals

--- config.sh
+++ config.sh
@@ -14,5 +14,5 @@
 # source -- "$(blesh-share)"/ble.sh # Uncomment this if you have ble.sh on your system
 # eval "$(starship init bash)" # Uncomment this eval if you have starship on your system
 # eval "$(zoxide init bash --cmd cd)" # Uncomment this if you have zoxide on your system
-# eval $(thefuck --alias) # Uncomment this if you have thefuck on your your system
 # eval "$(pay-respects bash --alias)" # Uncomment this if you have pay-respects on your system
+# eval $(thefuck --alias) # Uncomment this if you have thefuck on your your system
