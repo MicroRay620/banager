407d02ee7662e4b7093d8980f8d253460ff094b3
Ruby Rose <microray620@gmail.com>
2026-07-26 20:18:00 -0400

    Removed the plugin check

--- deps/config.sh
+++ deps/config.sh
@@ -24,51 +24,48 @@ for cmd_file in "$data_dir"/commands/*; do
    fi
 done
 # Plugins
-# This is for managing the plugins
 # shellcheck disable=SC2154
-if [ "$plugin" = "enable" ]; then
-    if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins" ]; then 
-        mkdir "$config_dir/plugins"
-    fi
-    for plugin_file in "$config_dir"/plugins/*.plugin*; do
-        plugin_path="${plugin_file##*\/}"
-        invalid_plugin="${plugin_path%%.plugin.sh}"
-        if [ -f "$plugin_file" ]; then 
-            # NOTE: This is here for debugging. 
-            # Uncomment this if you want to test your plugin
-            # echo "Reading $invalid_plugin.plugin.sh"
-            owner=$(cat "$plugin_file" | grep "# owner =")
-            shebang=$(cat "$plugin_file" | grep "#!banager/plugin")
-            if [ -n "$owner" ]; then
-                # shellcheck source=/dev/null
-                source "$plugin_file"
-                # NOTE: This is here for debugging. Uncomment this if you are debugging a plugin for Banager
-                # echo -e "$invalid_plugin is valid"
-                # echo -e "#!banager/plugin exists in $invalid_plugin"
-            else
-                cat "$plugin_file"
-                echo "$invalid_plugin is invalid"
-                if [ -z "$shebang" ]; then 
-                    # NOTE: This checks for #!banger/plugin to see if a plugin is valid
-                    echo -e "\e[31m Cause: BC1: $invalid_plugin.plugin.sh's #!banager/plugin is missing\e[0m"
-                fi
-                if [ -z "$owner" ]; then 
-                    echo -e "\e[31m Cause: BC2: $invalid_plugin.plugin.sh's 'owner' field is empty\e[0m"
-                else 
-                    echo -e "\e[31mCause: BC1: $invalid_plugin.plugin.sh's 'owner' field is missing\e[0m"
-                fi
+if [ ! -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins" ]; then 
+    mkdir "$config_dir/plugins"
+fi
+for plugin_file in "$config_dir"/plugins/*.plugin*; do
+    plugin_path="${plugin_file##*\/}"
+    invalid_plugin="${plugin_path%%.plugin.sh}"
+    if [ -f "$plugin_file" ]; then 
+        # NOTE: This is here for debugging. 
+        # Uncomment this if you want to test your plugin
+        # echo "Reading $invalid_plugin.plugin.sh"
+        owner=$(cat "$plugin_file" | grep "# owner =")
+        shebang=$(cat "$plugin_file" | grep "#!banager/plugin")
+        if [ -n "$owner" ]; then
+            # shellcheck source=/dev/null
+            source "$plugin_file"
+            # NOTE: This is here for debugging. Uncomment this if you are debugging a plugin for Banager
+            # echo -e "$invalid_plugin is valid"
+            # echo -e "#!banager/plugin exists in $invalid_plugin"
+        else
+            cat "$plugin_file"
+            echo "$invalid_plugin is invalid"
+            if [ -z "$shebang" ]; then 
+                # NOTE: This checks for #!banger/plugin to see if a plugin is valid
+                echo -e "\e[31m Cause: BC1: $invalid_plugin.plugin.sh's #!banager/plugin is missing\e[0m"
+            fi
+            if [ -z "$owner" ]; then 
+                echo -e "\e[31m Cause: BC2: $invalid_plugin.plugin.sh's 'owner' field is empty\e[0m"
+            else 
+                echo -e "\e[31mCause: BC1: $invalid_plugin.plugin.sh's 'owner' field is missing\e[0m"
             fi
         fi
-    done
-    if [ -f "$config_dir/plugins/fetch.plugin.sh" ]; then 
-        if [ "$flag_display" = "true" ]; then 
-            Flag
-        fi
     fi
-    if [ -f "$config_dir/plugins/gtrash.plugin.sh" ]; then 
-        touch "$cache_dir/gtrash.sh" 
-        if command -v gtrash &>/dev/null; then 
-            gtrash completion bash >> "$cache_dir/gtrash.sh"
-        fi
+done
+if [ -f "$config_dir/plugins/fetch.plugin.sh" ]; then 
+    if [ "$flag_display" = "true" ]; then 
+        Flag
+    fi
+fi
+if [ -f "$config_dir/plugins/gtrash.plugin.sh" ]; then 
+    touch "$cache_dir/gtrash.sh" 
+    if command -v gtrash &>/dev/null; then 
+        gtrash completion bash >> "$cache_dir/gtrash.sh"
     fi
 fi
