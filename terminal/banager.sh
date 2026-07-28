ff588785880fbf440f0adff2efcab2ed9dc46b63
Ruby Rose <microray620@gmail.com>
2026-07-28 13:05:43 -0400

    Hopefully have the command working a bit better

--- terminal/banager.sh
+++ terminal/banager.sh
@@ -7,8 +7,7 @@ export bastore="$HOME/.local/share/banager/run"
 alias banare='$locate_source'
 configs=$(getopts -o c,h,p --long config,help,plugins --name "$0" -- "$@")
 eval set -- "$configs"
-grabs=("curl" "wget") 
-for grab in "${grabs[@]}"; do
+for grab in curl wget; do
     if command -v "$grab"; then 
         get="$grab"
     fi
@@ -21,21 +20,57 @@ banager() {
                 exit 0
                 ;;
             "-h" | "--help") 
-                echo "work in progress" 
+                echo "work in progress"
                 exit 0
                 ;; # This will be a function
             "-p" | "--plugins") 
-                echo -e "Installed plugins are:\n$(ls "$locate_config/plugins")" 
+                while true; do 
+                    case "$1" in 
+                        *list*) 
+                            echo -e "Installed plugins are:\n$(ls "$locate_config/plugins")" 
+                            shift 1
+                            ;;
+                        *install*) 
+                            place=0
+                            arg="$2"
+                            if [ -z "$arg" ] || [ "${arg:0:1}" = "-" ]; then 
+                                echo "Error: No plugin"
+                                echo "To install plugins please do: banager -p install <RAW PLUGIN FILE URL>"
+                            fi
+                            for gets in curl wget; do
+                                if [ "$place" = 0 ]; then 
+                                    flag="-O"
+                                elif [ "$place" = 1 ]; then 
+                                    flag="-P"
+                                else
+                                    echo "No flag avaliable"
+                                fi
+                                if command -v "$gets" &>/dev/null && [ ! "$place" -ge 1 ]; then 
+                                    $gets "$flag $arg"
+                                    break
+                                else 
+                                    (( place += 1 ))
+                                fi
+                            done
+                            shift 2
+                            ;;
+                        *remove*)
+                            echo "argument is in development"
+                            arg="$2"
+                            # Add a for loop that will show all the possible plugins that can be removed
+                            shift 2
+                            ;;
+                        *)
+                            echo "BC1: Plugin Error 1: That flag argument doesn't exist"
+                    esac
+                done 
                 # Probably will make more options for this
                 # TODO: Make this able to install plugins and have a sub-argument
-                # This will use the $get variable:
-                # - First it will cd into $XDG_CONFIG_HOME (~/.config/banager/plugins)
-                # - Then it will pull the plugin from the url
-                # - Then it will exit 
                 exit 0
                 ;;
             "-u" | "--update" | "update") 
                 $get https://codeberg.org/RubyRose/banager/raw/branch/main/install.sh && sh install.sh && rm -rf ./install.sh 
+                shift 1
                 ;;
         esac
     done
