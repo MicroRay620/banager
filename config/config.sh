0ed618a3b234ffa629a16762ed14c8b345d3fb7a
Ruby Rose <microray620@gmail.com>
2026-07-27 22:35:47 -0400

    Moved config.sh to config/config.sh

---
+++ config/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Use plugins at your own risk
+# plugin=enabled # You need this enabled for plugins. This variable may be removed
+# shellcheck disable=SC2034
+# flat_alias=true
+# yt_alias=true
+# correction=true
+# show_logo=true # Needs: FastFetch Plugin 
+# distro_alias=true # REQUIRES: archlinux.plugin.sh or nixos.plugin.sh 
+# dirmember=true # REQUIRES: zoxide.plugin.sh 
+# starship=true # REQUIRES: starship.plugin.sh
+# Look at the docs for those tools for how to add them and look for the bash option.
+# eval "$(zoxide init bash --cmd cd)" # Uncomment this if you have zoxide on your system
