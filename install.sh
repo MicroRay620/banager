a7bf34f5feb15c97725fa641e2aba1c02841592e
Ruby Rose <microray620@gmail.com>
2026-07-28 13:29:01 -0400

    Updated the install.sh to have stable and unstable option

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
+# shellcheck source=/dev/null
 source -- /etc/os-release
 banampt=$(mktemp -d banampt.XXXXXX)
 cd "$banampt" || exit 1
 
-# supers=$("sudo" "doas")
-# for super in "${supers[@]}"; do 
-#    if $super &>/dev/null; then 
-#        SUPER="$super"
-#        break
-#    else 
-#        continue
-#    fi
-# done
+supers=$("sudo" "doas")
+for super in "${supers[@]}"; do 
+   if $super &>/dev/null; then 
+       SUPER="$super"
+       break
+   else 
+       continue
+   fi
+done
 echo "Checking dependencies..."
 if command -v bleopt &>/dev/null; then
     echo "ble.sh is installed, can continue"
@@ -28,52 +29,87 @@ else
     fi
     exit 2
 fi
-
-git clone https://codeberg.org/RubyRose/banager.git 
-
+# TODO: Add an input for the branch
+# Branch Options:
+# - stable (this will be just banager)
+# - unstable (in Arch this will be banager-git
+echo "Do you want [stable] or [unstable]? "
+read -r branch 
+case "$branch" in 
+    *unstable* | *UNSTABLE*) git clone --bare https://codeberg.org/RubyRose/banager.git ;;
+    *stable* | *STABLE*) git clone --bare https://codeberg.org/RubyRose/banager/src/stable.git ;;
+esac
 echo "Removing dev and useless files and folders..."
 rm -rf banager/install.sh banager/docs/
 
-echo "Making the data directory..."
-mkdir "${XDG_DATA_HOME:-$HOME/.local/share}/banager"
-echo "Made the banager data directory :)"
-echo "Making the config directory..."
-mkdir "${XDG_CONFIG_HOME:-$HOME/.config}/banager"
-echo "Making the plugin directory..."
-mkdir "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins"
-echo "Made the config directory :)"
-echo "Making the cache directory..." 
-mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager"
-echo "Making the user cache..."
-mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user"
-echo "Made the user cache :)"
-echo "Making the plugin cache..."
-mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins"
-echo "Made the cache directory."may not point to the repo root. Pr
-
-mv -fu ./banager/run/ ./banager/commands/ "$HOME"/.local/share/banager
-# echo "Install for [user] or [system]? "
-# echo "On NixOS system, it's recommended to do user"
-# read -r install_choice
-# echo "Adding the command..."
-# case "$install_choice" in 
-#    *user* | *USER*) mv -fu "$banampt"/commands/banager/banager.sh "$HOME/.local/bin/" ;;
-#    *sys*  | *SYS* ) $SUPER mv -fu "$banampt"/commands/banager/banager.sh /usr/local/bin ;;
-# esac
-# This command is temporary and will be removed once I get the installation working 
+if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager" ]; then 
+    echo "Making the data directory..."
+    mkdir "${XDG_DATA_HOME:-$HOME/.local/share}/banager"
+    echo "Made the banager data directory :)"
+    echo "Moving the data files..."
+    mv -fu ./banager/run/ ./banager/src/ "${XDG_DATA_HOME:-$HOME/.local/share}/banager"
+    echo "Moved data files"
+fi
+if [ ! -f "${XDG_CONFIG_HOME:-$HOME/.config}/banager" ]; then 
+    echo "Making the config directory..."
+    mkdir "${XDG_CONFIG_HOME:-$HOME/.config}/banager"
+    if [ ! -f "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins" ]; then
+        echo "Making the plugin directory..."
+        mkdir "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins"
+        echo "Made the plugin directory :)"
+    fi
+    echo "Made the config directory :)"
+fi
+if [ ! -f "${XDG_CACHE_HOME:-$HOME/.cache}/banager" ]; then 
+    echo "Making the cache directory..." 
+    mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager"
+    if [ ! -f "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user" ]; then
+        echo "Making the user cache..."
+        mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user"
+        echo "Made the user cache :)"
+    fi
+    if [ ! -f "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins" ]; then 
+        echo "Making the plugin cache..."
+        mkdir "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins"
+        echo "Made the plugin cache :)"
+    fi
+    echo "Made the cache directory."
+fi
+echo "Install for [user] or [system]? "
+echo "On NixOS system, it's recommended to do user"
+read -r install_choice
+echo "Adding the command..."
+case "$install_choice" in 
+    *user* | *USER*) 
+        if [ ! -e "${XDG_BIN_HOME:-$HOME/.local/bin}/banager.sh" ]; then 
+            echo "Adding command to user..."
+            mv -fu "$banampt"/src/banager/banager.sh "${XDG_BIN_HOME:-$HOME/.local/bin}" 
+            echo "Added command to user :)"
+        else 
+            echo "The command is already installed on the user"
+        fi
+        ;;
+    *sys*  | *SYS* ) 
+        if [ ! -e "/usr/local/bin" ]; then
+            echo "Adding command to system..."
+            $SUPER mv -fu "$banampt"/src/banager/banager.sh /usr/local/bin 
+            echo "Added command to system :)"
+        else 
+            echo "Command is already on the system"
+        fi
+        ;;
+esac
 mv -fu ./banager/config/config.sh "${XDG_CONFIG_HOME:-$HOME/.config}"
 if [ -e "$HOME/.bashrc" ]; then 
     mv -fu "$HOME"/.bashrc "$HOME"/.bashrc.bak
     echo "Made .bashrc.bak"
 fi
 touch "$HOME"/.bashrc
-# The banager_config="$XDG_DATA_HOME/banager/run/" source -- "$banager_config"/config.sh is temporary until I can get $banater to work in the source
 echo -e "#!/usr/bin/env bash
 \nbanager_config=\"\$XDG_DATA_HOME/banager/run/\"\n
 # shellcheck source=/dev/null
-\nsource -- \"\$XDG_DATA_HOME/banager/run/config.sh\"" >> "$HOME"/.bashrc
+\nsource -- \"\${XDG_DATA_HOME:-\$HOME/.local/share}/banager/run/config.sh\"" >> "$HOME"/.bashrc
 cd ../ || exit 1
 rm -rf "$banampt"
-
 echo "Installed."
 exit 0
