efe9dc153c64509550f9ab027b02185ee2908624
Ruby Rose <microray620@gmail.com>
2026-07-28 22:52:39 -0400

    Moved some of the config options to base

--- config/config.sh
+++ config/config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # Use plugins at your own risk
-# plugin=enabled # You need this enabled for plugins. This variable may be removed
 # shellcheck disable=SC2034
-# flat_alias=true
 # yt_alias=true
 # correction=true
+# starship=true 
+# dirmember=true  
 # show_logo=true # REQUIRES: FastFetch Plugin 
 # distro_alias=true # REQUIRES: archlinux.plugin.sh or nixos.plugin.sh 
-# dirmember=true # REQUIRES: zoxide.plugin.sh 
-# starship=true # REQUIRES: starship.plugin.sh
-# Look at the docs for those tools for how to add them and look for the bash option.
-# eval "$(zoxide init bash --cmd cd)" # Uncomment this if you have zoxide on your system
+
+# This is a user file that will be made to have custom user aliases
+# shellcheck source=/dev/null
+source "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh"
