a2f86e85ed025b586a9fece47501c8028931ce75
Ruby Rose <microray620@gmail.com>
2026-07-29 18:14:28 -0400

    Made an update script

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash 
+# This is a file that uses things from install.sh 
+# shellcheck source=/dev/null
+source -- "${XDG_CACHE_HOME:-$HOME/.cache}/banager/install/update.storage.sh"
+# shellcheck disable=SC2154
+git clone --bare "https://codeberg.org/RubyRose/banager/src/branch/$installed_branch"
+
+if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/" ]; then 
+    if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run" ]; then 
+        cp -fru ./banager/run "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
+    fi
+    if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src" ]; then 
+        cp -fru ./banager/src "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
+    fi
+fi
+if [ -e "${XDG_BIN_HOME:-$HOME/.local/bin}/banager" ]; then 
+    cp -fu ./banager/terminal/banager "${XDG_BIN_HOME:-$HOME/bin}/banager"
+fi
+if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/banager" ]; then 
+    if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/config.sh" ]; then 
+        cp -fu ./banager/config/config.sh "${XDG_CONFIG_HOME:-$HOME/.config}/banager"
+    fi
+    if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh" ]; then 
+        cp -fu ./banager/config/usr-alias "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh"
+    fi
+fi
