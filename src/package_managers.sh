53d71aef19e391197a23d518395294a51d4c4ea6
Ruby Rose <microray620@gmail.com>
2026-07-29 17:26:33 -0400

    Added a SKIP_CONF and made it to be in UPDATE

--- src/package_managers.sh
+++ src/package_managers.sh
@@ -19,18 +19,20 @@ case "$ID" in
 esac
 case "$PKG_MGR" in 
     pacman)
+        SKIP_CONF="--noconfirm"
         INSTALL="-Sv --needed"
         REMOVE="-Rsu"
-        UPDATE="--noconfirm -Syu --needed"
+        UPDATE="$SKIP_CONF -Syu --needed"
         # shellcheck disable=SC2034
         IGNORE="--ignore"
         ;;
     *)
+        SKIP_CONF="-y"
         # shellcheck disable=SC2034
         INSTALL="install"
         # shellcheck disable=SC2034
         REMOVE="remove"
         # shellcheck disable=SC2034
-        UPDATE="update -y"
+        UPDATE="update $SKIP_CONF"
         ;;
 esac
