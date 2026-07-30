bd1ef6c842e9d6f439deda107aaa303f2211d43e
Ruby Rose <microray620@gmail.com>
2026-07-29 23:27:15 -0400

    Made it so the update script uses the --update=all flag

--- update.sh
+++ update.sh
@@ -8,20 +8,25 @@ git clone --bare "https://codeberg.org/RubyRose/banager.git"
 
 if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/" ]; then 
     if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run" ]; then 
-        cp -fru ./banager/run "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
+        cp -fr --update=all ./banager/run "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
     fi
     if [ -f "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src" ]; then 
-        cp -fru ./banager/src "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
+        cp -fr --update=all ./banager/src "${XDG_DATA_HOME:-$HOME/.local/share}/banager" 
     fi
 fi
 if [ -e "${XDG_BIN_HOME:-$HOME/.local/bin}/banager" ]; then 
-    cp -fu ./banager/terminal/banager "${XDG_BIN_HOME:-$HOME/bin}/banager"
+    cp -fr --update=all ./banager/terminal/banager "${XDG_BIN_HOME:-$HOME/bin}/banager"
 fi
 if [ -f "${XDG_CONFIG_HOME:-$HOME/.config}/banager" ]; then 
     if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/config.sh" ]; then 
-        cp -fu ./banager/config/config.sh "${XDG_CONFIG_HOME:-$HOME/.config}/banager"
+        cp -fr --update=all ./banager/config/config.sh "${XDG_CONFIG_HOME:-$HOME/.config}/banager"
     fi
     if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh" ]; then 
-        cp -fu ./banager/config/usr-alias "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh"
+        cp -fr --update=all ./banager/config/usr-alias "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh"
     fi
+fi
+if [ -e "${XDG_CONFIG_HOME:-$HOME/.config}/banager/plugins/gtrash.plugin.sh" ]; then 
+    gtrash rm banager 
+else 
+    rm -rf banager
 fi
