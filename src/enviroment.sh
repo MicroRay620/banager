892a0673e7d7060615cfcfb4db85cf7f3a721e16
Ruby Rose <microray620@gmail.com>
2026-08-03 12:08:32 -0400

    Added some more things to enviroment.sh
    Such things are like a cache path with a new file name and a comment
    documentation for the XDG variable

--- src/enviroment.sh
+++ src/enviroment.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck source=/dev/null
 source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/declare.sh"
-enviroment_path="${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/env.sh"
-if [ ! -e "$enviroment_path" ]; then
+if [ ! -e "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/enviroment.path.save.sh" ]; then
     cd "$XDG_CONFIG_HOME"/banager/user/ || exit 1
     cd "$HOME" || return
     touch "env.sh"
@@ -12,23 +11,20 @@ if [ ! -e "$enviroment_path" ]; then
         text_editor="nvim"
     fi
     # shellcheck disable=SC2154
-    echo -e "$bash_declare\n$bash_gen\n$dont_delete\nexport text=$text_editor" >> "$enviroment_path"
+    echo -e "$bash_declare\n$bash_gen\n$dont_delete\nexport text=$text_editor" >> "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/enviroment.path.save.sh"
 fi
 # shellcheck source=/dev/null
-source -- "$enviroment_path"
-# Env vars 
+source -- "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/enviroment.path.save.sh"
 export RUST_BACKTRACE=1
 export HISTFILE=~/.bash_history
 export HISTSIZE=1000
 export HISTFILESIZE=2000
 # shellcheck disable=SC2154
 export EDITOR="$text" 
-export VISUAL=""
-
+# These are standard XDG variables
+# It's not meant to change
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_BIN_HOME="$HOME/.local/bin"
-
-export bastore="$HOME/.local/share/banager/deps"
