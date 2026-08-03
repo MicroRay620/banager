20631e905982e25eb003f0ad47da93ab47fbc30b
Ruby Rose <microray620@gmail.com>
2026-08-03 12:03:50 -0400

    Removed the source to usr-alias and the variables that needed external plugins
    The usr-alias (now known as alias.sh) is sourced in ./run/config.sh.
    The external plugin variables are now being removed from banager. They
    should be default in the plugins and not need to be enabled in the
    config file

--- config/config.sh
+++ config/config.sh
@@ -5,9 +5,4 @@
 # correction=true
 # starship=true 
 # dirmember=true  
-# show_logo=true # REQUIRES: FastFetch Plugin 
-# distro_alias=true # REQUIRES: archlinux.plugin.sh or nixos.plugin.sh 
-
-# This is a user file that will be made to have custom user aliases
-# shellcheck source=/dev/null
-source "${XDG_CONFIG_HOME:-$HOME/.config}/banager/usr-alias.sh"
+# time=true 
