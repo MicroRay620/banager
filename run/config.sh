2b409a53f31e23f21bb7741824fc72a3e7627c91
Ruby Rose <microray620@gmail.com>
2026-08-11 12:34:35 -0400

    refactor: made speed and added log file

--- run/config.sh
+++ run/config.sh
@@ -1,34 +1,55 @@
 #!/usr/bin/env bash
-# shellcheck source=/dev/null
-if [ -e "${XDG_BIN_HOME:-$HOME/.local/bin}/banager" ]; then 
-    source -- "${XDG_BIN_HOME:-$HOME/.local/bin}/banager"
-elif [ -e "/usr/local/bin/banager" ]; then 
-    source -- /usr/local/bin/banager 
-fi
+# shellcheck disable=SC2154 source=/dev/null
 date=$(date +"%Y.%m.%d")
 if [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs/banager-$date.log" ]; then 
     touch "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs/banager-$date.log"
     date +"%T">> "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs/banager-$date.log"
-else 
-    date +"%T" >> "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs/banager-$date.log"
 fi
+# This just checks for command level
+if [ -e "${XDG_BIN_HOME:-$HOME/.local/bin}/banager" ]; then 
+    source -- "${XDG_BIN_HOME:-$HOME/.local/bin}/banager"
+    echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: ${XDG_BIN_HOME:-$HOME/.local/bin} banager command installed at user level" >> "$log_file"
+elif [ -e "/usr/local/bin/banager" ]; then 
+    source -- "/usr/local/bin/banager"
+    echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: /usr/local/bin/banager: banager command installed at system level" >> "$log_file"
+else 
+    echo -e "\e[31mBC1 Error: banager command is missing\e[0m"
+    echo -e "\e[31m${XDG_DATA_HOME:-$HOME/.local/share/}/banager/run/config.sh: Error BC1: banager command not installed\e[0m"
+    exit 1
+fi
+source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/enviroment.sh"
 source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/alias.sh"
 source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/builtin.plugin.sh"
-source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/enviroment.sh"
 source -- "${XDG_DATA_HOME:-$HOME/.local/share}/banager/src/declare.sh"
-source -- "${XDG_CONFIG_HOME:-$HOME/.config}/banager/config.sh"
-source -- "${XDG_CONFIG_HOME:-$HOME/.config}/banager/alias.sh"
+if [ ! -e "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/log.storage.time.sh" ]; then
+    echo "How long would you like to store the logs? (time in terms of days) "
+    read -r log_store 
+    echo -e "$bash_declare\n$bash_gen\n$dont_delete\nlog_store=$log_store" >> "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/log.storage.time.sh" 
+    source -- "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/log.storage.time.sh"
+    echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: log storage cache file not found: created log storage" >> "$log_file"
+else
+    echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: log storage cache file found" >> "$log_file"
+    source -- "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/log.storage.time.sh"
+fi
+config_check=$(shellcheck -x "${XDG_CONFIG_HOME:-$HOME/.config}/banager/config" &>/dev/null)
+if [ ! "$config_check" = "" ]; then 
+    echo -e "\e[31mERROR BC3.1: config file failed shellcheck\e[0m"
+    echo -e "\e[31m${XDG_CONFIG_HOME:-$HOME/.config}/banager/config: Error BC3: failed config file: failed shellcheck\e[0m" >> "$log_file"
+else
+    echo ":)"
+fi
+# TODO: Make the log deletion logic
+source -- "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user/log.storage.time.sh"
+source -- "${XDG_CONFIG_HOME:-$HOME/.config}/banager/config"
 # All of these are to abide by the XDG standard
 # For more about these please go to: https://specifications.freedesktop.org/basedir/latest/
 # NOTE: This is just making the cache directory
 if [ ! -e "${XDG_CACHE_HOME:-$HOME/.cache}/banager" ] || [ ! -e "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs" ] ; then 
-    mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}/banager" "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user"\
-        "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins" "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs"
+    mkdir -p "${XDG_CACHE_HOME:-$HOME/.cache}/banager/user" "${XDG_CACHE_HOME:-$HOME/.cache}/banager/plugins" "${XDG_DATA_HOME:-$HOME/.local/share}/banager/logs"
 fi
 
 # Plugins
 # This is for managing the plugins
-# shellcheck disable=SC2154
 for plugin_file in "${XDG_CONFIG_HOME:-$HOME/.config}/banager"/plugins/*.plugin*; do
     plugin_path="${plugin_file##*\/}"
     invalid_plugin="${plugin_path%%.plugin.sh}"
@@ -41,7 +62,6 @@ for plugin_file in "${XDG_CONFIG_HOME:-$HOME/.config}/banager"/plugins/*.plugin*
         shebang=$(echo "$plugin" | grep "#!banager/plugin")
         owner=$(echo "$plugin" | grep "# owner = ")
         if [[ "$plugin" =~ "#!banager/plugin" ]] && [[ "$plugin" =~ "# owner = " ]]; then
-            # shellcheck source=/dev/null
             source "$plugin_file"
             echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: $plugin_file is enabled" >> "$log_file"
             # NOTE: This is here for debugging. Uncomment this if you are debugging a plugin for Banager
@@ -54,14 +74,14 @@ for plugin_file in "${XDG_CONFIG_HOME:-$HOME/.config}/banager"/plugins/*.plugin*
             if [ -z "$shebang" ]; then 
                 # NOTE: This checks for #!banger/plugin to see if a plugin is valid
                 echo -e "\e[31m Cause: BC1: $invalid_plugin.plugin.sh's #!banager/plugin is missing\e[0m"
-                echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: $plugin_file is missing #!banager/plugin" >> "$log_file"
+                echo -e "\e[31m${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: Error BC1: $plugin_file is missing #!banager/plugin\e[0m" >> "$log_file"
             fi
             if [ -z "$owner" ]; then 
                 echo -e "\e[31m Cause: BC2: $invalid_plugin.plugin.sh's 'owner' field is empty\e[0m"
-                echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: $plugin_file is owner field is empty" >> "$log_file"
+                echo -e "\e[31m${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: Error BC2: $plugin_file is owner field is empty\e[0m" >> "$log_file"
             else 
                 echo -e "\e[31mCause: BC1: $invalid_plugin.plugin.sh's 'owner' field is missing\e[0m"
-                echo "${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: $plugin_file is owner field is missing" >> "$log_file"
+                echo  -e "\e[31m${XDG_DATA_HOME:-$HOME/.local/share}/banager/run/config.sh: Error BC1: $plugin_file is owner field is missing\e[0m" >> "$log_file"
             fi
         fi
     fi
